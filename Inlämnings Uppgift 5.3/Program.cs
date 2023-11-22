68a50d6b18d0a3180ef0b531d9d2c9206fe1995c
sere06001 <sere06001@utb.vaxjo.se>
2023-11-22 09:57:36 +0100

    Add project files.

---
+++ Inlämnings Uppgift 5.3/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace Uppgift5_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Skriv in tre heltal på en rad");
+            string[] text = Console.ReadLine().Split();
+            int tal1 = int.Parse(text[0]);
+            int tal2 = int.Parse(text[1]);
+            int tal3 = int.Parse(text[2]);
+            Console.WriteLine("Summan av talen är: "+(tal1+tal2 + tal3));
+            Console.ReadKey();
+        }
+    }
+}
